1fd5c2c3e90b7a7a7bd9dbb99da7e3b24dec6597
Leon Mak <leonmakansheng@gmail.com>
2018-05-21 23:48:06 -0700

    Add chapter 2 assignments

---
+++ assignments/2_udp_pinger.go
@@ -0,0 +1,9 @@
+// - Find the delay between when the client sent the ping message and received the pong message - Round Trip Time (RTT)
+// - Send 10 ping messages to the target server over UDP.
+// - To not wait forever for lost packets, assume lost after 1 sec, and print a message.
+// - Note `ping` command uses ICM not UDP
+package main
+
+func main() {
+
+}
---
+++ assignments/3_smtp_mail_cilent.go
@@ -0,0 +1,2 @@
+// Establish a TCP connection with the mail server
+package main
